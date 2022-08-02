39d1b4438ccbd1c9c2547179e6b77212af3a2344
gulbetulsrky <94078566+gulbetulsrky@users.noreply.github.com>
2022-08-02 22:36:59 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -52,13 +52,12 @@ if st.button('PREDICT'):
 	if prediction[0]==0:
 		st.success(prediction[0])
 		st.success(f'It is a non fraudulent transaction.')
-		st.markdown("![Alt Text](https://media4.giphy.com/media/hgjNPEmAmpCMM/giphy.gif?cid=ecf05e47avsfwy34clng4aqdpx53bg2a81m6x7xboop6sa3g&rid=giphy.gif&ct=g)")
-
+		
 
 	elif prediction[0]==1:
 		st.warning(prediction[0])
 		st.warning(f'It is a fraudulent transaction.')
-
+		st.markdown("![Alt Text](https://media4.giphy.com/media/hgjNPEmAmpCMM/giphy.gif?cid=ecf05e47avsfwy34clng4aqdpx53bg2a81m6x7xboop6sa3g&rid=giphy.gif&ct=g)")
 
 hide_streamlit_style = """
             <style>
