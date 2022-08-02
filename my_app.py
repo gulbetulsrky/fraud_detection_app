fe50baa63826339e282756cf0dfdd05586ea934a
gulbetulsrky <94078566+gulbetulsrky@users.noreply.github.com>
2022-08-02 22:03:23 +0300

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -30,7 +30,7 @@ v14 = st.sidebar.slider("v14", min_value=-20.00, max_value=11.00, step=0.01)
 v16 = st.sidebar.slider("v16", min_value=-15.00, max_value=18.00, step=0.01)
 v17 = st.sidebar.slider("v17", min_value=-26.00, max_value=10.00, step=0.01)
 
-# fraud = pd.DataFrame({‘v14’:v14, ‘v17’:v17, ‘v12’:v12, ‘v10’:v10, ‘v11’:v11, ‘v4’:v4, ‘v3’:v3, ‘v7’:v7, ‘v16’:v16})
+fraud = pd.DataFrame({‘v14’:v14, ‘v17’:v17, ‘v12’:v12, ‘v10’:v10, ‘v11’:v11, ‘v4’:v4, ‘v3’:v3, ‘v7’:v7, ‘v16’:v16})
 
 hide_table_row_index = """
             <style>
